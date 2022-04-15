9e2f0515e29db289e3fbe9e065ddc4fe07d94d9a
vaibhavkapur22 <vaibhavkapur22@gmail.com>
2022-04-15 09:39:34 -0400

    Step-1 on Flask

---
+++ static/home.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+  $("#nav_home").addClass("active");
+
+  $("#nav_learn").removeClass("active");
+  $("#nav_game").removeClass("active");
+
+    $("#start").click(function(){
+        window.location.href="/learn/1";
+
+      })
+
+
+
+
+});
